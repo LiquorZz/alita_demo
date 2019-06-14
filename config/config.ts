eca5bf3641c094effb8958793271d75a6127cd50
mao xiangming <mxm__99068@163.com>
2019-06-14 16:17:31 +0800

    config修改

--- config/config.ts
+++ config/config.ts
@@ -4,19 +4,16 @@ export default {
     '/mock/70943': {
       target: 'http://yapi.demo.qunar.com',
       changeOrigin: true,
-      // "pathRewrite": { "/api/specialOperation/rest.json": "" }
     },
     // 腾讯地图api
     '/ws/district/v1': {
       target: 'https://apis.map.qq.com',
       changeOrigin: true,
-      // "pathRewrite": { "/api/specialOperation/rest.json": "" }
     },
     // 高德地图api
     '/v3/config/district': {
       target: 'https://restapi.amap.com',
       changeOrigin: true,
-      // "pathRewrite": { "/api/specialOperation/rest.json": "" }
     },
   },
 };
